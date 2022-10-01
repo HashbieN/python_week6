ff831f1b412d9dc78e423f1ee860102883a083d7
HashbieN <20DDT20F2028@student.pmu.edu.my>
2022-10-01 16:44:16 +0800

    uwu

---
+++ Exercise2/Question1_SquareCalculator.py
@@ -0,0 +1,23 @@
+# The Python Square Calculator (Command Line Edition)
+# by Hashbie Nazray Nazarinie bin Mohammad (20DDT20F2028)
+
+# DFP40203 Python Programming
+# In completion of Diploma in Information Technology
+# Supervised by En. Muhamad Faizal bin Md Zulkifli
+
+# Copyright (C) 2022 Politeknik Mukah. All rights reserved
+
+
+# START OF CODE
+
+sq = 1  # square
+
+print("\nThe Python Square Calculator\n")
+
+for sq in range(1, 11):  # loops from index 1 to 10 because end = end-1
+
+    square = sq ** 2
+
+    print("The square of", sq, "is:", square)
+
+# END OF CODE
